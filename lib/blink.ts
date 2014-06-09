f2f1d1dfadc33063e603b9c9e57368a284da2450
Jed Mao <jed.hunsaker@gmail.com>
2014-06-09 00:45:03 -0500

    Implement blink

---
+++ lib/blink.ts
@@ -0,0 +1,63 @@
+﻿///<reference path="../node_modules/blink/blink.d.ts"/>
+
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+import blink = require('blink');
+
+// Consts
+var PLUGIN_NAME = 'gulp-blink';
+
+// Plugin level function(dealing with files)
+function gulpBlink(options) {
+
+	options = options || {};
+
+	// Creating a stream through which each file will pass
+	var stream = through.obj(function(file, enc, callback) {
+
+		var onBlinkResultCompiled = (err, config, result) => {
+			if (err) {
+				this.emit('error', new PluginError(PLUGIN_NAME, err.message));
+			}
+			file.contents = new Buffer(result.contents);
+			file.path = result.dest;
+			this.push(file);
+			callback();
+		};
+
+		switch (true) {
+			
+			case file.isNull():
+				// do nothing
+				break;
+			
+			case file.isStream():
+				file.contents.path = file.path;
+				blink.compileStream(options, file.contents, onBlinkResultCompiled);
+				return;
+
+			case file.isBuffer():
+				blink.compileContents(options, {
+					src: file.path,
+					contents: file.contents.toString()
+				}, onBlinkResultCompiled);
+				return;
+			
+			default:
+				this.emit('error', new PluginError(PLUGIN_NAME, 'Unexpected file type'));
+				break;
+		}
+
+		this.push(file);
+		callback();
+
+	});
+
+	// returning the file stream
+	return stream;
+};
+
+// Exporting the plugin main function
+export = gulpBlink;
---
+++ test/spec/lib/blink.spec.ts
@@ -0,0 +1,31 @@
+﻿import fs = require('fs');
+
+import sinonChai = require('../../sinon-chai');
+var expect = sinonChai.expect;
+
+
+// ReSharper disable WrongExpressionStatement
+describe('gulp-blink', () => {
+
+	['streaming', 'buffer'].forEach(mode => {
+		describe(mode + ' mode', () => {
+
+			['foo', 'bar'].forEach(foobar => {
+				it('compiles expected ' + foobar + ' output', done => {
+
+					fs.readFile('tmp/test/actual/stream/' + foobar + '.css', (err, actual) => {
+						if (err) throw err;
+
+						fs.readFile('test/expected/' + foobar + '.css', (err2, expected) => {
+							if (err2) throw err2;
+
+							expect(actual.toString()).to.eq(expected.toString());
+							done();
+						});
+					});
+				});
+			});
+		});
+	});
+
+});
