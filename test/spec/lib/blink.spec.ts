570b244158d1d67e334269f85064249173dc9c83
Jed Mao <jed.hunsaker@gmail.com>
2014-06-15 11:41:21 -0500

    Release 0.1-alpha

--- test/spec/lib/blink.spec.ts
+++ test/spec/lib/blink.spec.ts
@@ -14,10 +14,14 @@ describe('gulp-blink', () => {
 				it('compiles expected ' + foobar + ' output', done => {
 
 					fs.readFile('tmp/test/actual/stream/' + foobar + '.css', (err, actual) => {
-						if (err) throw err;
+						if (err) {
+							throw err;
+						}
 
 						fs.readFile('test/expected/' + foobar + '.css', (err2, expected) => {
-							if (err2) throw err2;
+							if (err2) {
+								throw err2;
+							}
 
 							expect(actual.toString()).to.eq(expected.toString());
 							done();
